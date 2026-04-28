641565336ac2d1b334a8dcba91d758576732bef6
Dumindu Dulanjaya <dulanjaya420@gmail.com>
2026-04-28 10:49:44 +0530

    Polish footer UI: add dividers, highlight phone, button style CTA, and micro animations

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -39,9 +39,9 @@ function Footer() {
                     <div className="footer-col resources-col">
                         <h4 className="col-title">Resources</h4>
                         <nav className="footer-nav-list">
-                            <a href="#" className="download-link">
-                                <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
-                                Company Profile (PDF)
+                            <a href="#" className="download-link primary-download">
+                                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+                                Download Company Profile
                             </a>
                             <a href="#" className="download-link">
                                 <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
@@ -50,6 +50,8 @@ function Footer() {
                             <Link to="/case-studies">Case Studies</Link>
                             <Link to="/knowledge-centre">Knowledge Centre</Link>
                         </nav>
+
+                        <div className="insights-divider"></div>
 
                         <h4 className="col-title mt-articles">Latest Insights</h4>
                         <ul className="footer-articles">
@@ -66,8 +68,12 @@ function Footer() {
                     <div className="footer-col contact-col">
                         <h4 className="col-title">Contact</h4>
                         <div className="contact-details">
-                            <a href="tel:+94771234567" className="contact-link">+94 77 123 4567</a>
-                            <a href="mailto:info@sanota.lk" className="contact-link">info@sanota.lk</a>
+                            <a href="tel:+94771234567" className="contact-link phone-link">
+                                📞 +94 77 123 4567
+                            </a>
+                            <a href="mailto:info@sanota.lk" className="contact-link">
+                                ✉️ info@sanota.lk
+                            </a>
                         </div>
                         
                         <h4 className="col-title mt-social">Connect With Us</h4>
