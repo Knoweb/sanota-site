71c72b81c3efc5aa7ceab6b46b8a4a8ff34ee448
Dumindu Dulanjaya <dulanjaya420@gmail.com>
2026-04-29 16:52:37 +0530

    Fix: Resolve tag mismatch in Solutions.jsx by re-adding missing section opening tag

--- src/pages/Solutions/Solutions.jsx
+++ src/pages/Solutions/Solutions.jsx
@@ -278,8 +278,8 @@ const Solutions = () => {
                         </motion.div>
                     </div>
                 </div>
-            </section>
-
+            {/* 4. CAPABILITY DETAILS (Slider) */}
+            <section className="solutions-capabilities section-padding light-theme">
                 <div className="container">
                     <div className="section-header centered">
                         <motion.h2 
